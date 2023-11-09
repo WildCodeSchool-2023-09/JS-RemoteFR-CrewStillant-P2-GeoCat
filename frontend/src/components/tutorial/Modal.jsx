46593baaddd7d19632d7d4a0fc8662e850d79f0c
sdiakhate <liousadiakhate20@gmail.com>
2023-11-09 15:51:37 +0100

    Fix the modal text: include(nat, une petite chatte           qui se promène dans les différents arrondissements de Paris)

--- frontend/src/components/tutorial/Modal.jsx
+++ frontend/src/components/tutorial/Modal.jsx
@@ -11,10 +11,11 @@ function Modal({ setModal }) {
       <div className="modalContainer">
         <h1>Fonctionnement du jeu</h1>
         <p>
-          Vous avez jusqu'à 3 clics pour géolocaliser notre chat parmi les 24
-          fuseaux horaires. <br /> Vous aurez des indices en haut de votre page,
-          pour vous aider à le retrouver. <br /> Vous trouverez aussi sur le bas
-          de votre page le nombre de clics qui vous restent.{" "}
+          Vous avez jusqu'à 3 clics pour géolocaliser Anat, une petite chatte
+          qui se promène dans les différents arrondissements de Paris <br />{" "}
+          Vous aurez des indices en haut de votre page, pour vous aider à le
+          retrouver. <br /> Vous trouverez aussi sur le bas de votre page le
+          nombre de clics qui vous restent.{" "}
         </p>
         <button type="button" onClick={closeModal}>
           X
