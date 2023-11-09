2aeeeef93da3929ea2e8175bf8aeb49d846ee74c
sdiakhate <liousadiakhate20@gmail.com>
2023-11-09 10:48:14 +0100

    delete the import GameTutorial in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import GameTutorial from "./components/tutorial/GameTutorial";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
-      <GameTutorial />
     </div>
   );
 }
